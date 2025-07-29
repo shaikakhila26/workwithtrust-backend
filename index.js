7a8570d90b89c255dde777289b482296ccc964b4
shaikakhila26 <akhilashaik2605@gmail.com>
2025-07-30 00:07:50 +0530

    updated 5000 to port

--- index.js
+++ index.js
@@ -268,4 +268,7 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(5000, () => console.log('Server running on port 5000 with Socket.IO'));
+
+const PORT =process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log('Server running on port 5000 with Socket.IO'));
